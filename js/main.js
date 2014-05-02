e05c4dc88027379dc6d9fa94b5be1561ed162f26
Neil MacMunn <neil@gumstix.com>
2014-05-01 21:58:20 -0700

    Preload images.

--- js/main.js
+++ js/main.js
@@ -14,12 +14,28 @@
 
     };
 
+    var onload = function () {
+        cnt += 1;
+        if (cnt === 2) {
+            $('html').addClass('ready');
+        }
+    };
+
+    /*
+     * Preload images
+     */
+    var cnt = 0;
+
+    var imgs = [new Image, new Image];
+    imgs[0].onload = onload;
+    imgs[1].onload = onload;
+
+    imgs[0].src = "img/background.jpg";
+    imgs[1].src = "img/TRwebLogo.svg";
+
     $(document).ready(function () {
-
         setPadding()
-
         $(window).on('resize', setPadding);
-
     });
 
 }());
