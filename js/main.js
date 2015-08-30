2441f797e684da0b8c1e6b089937ff7d1223822f
Neil MacMunn <neil@macmunn.com>
2015-08-29 19:17:31 -0700

    Blur the background and add a slight background transition.

--- js/main.js
+++ js/main.js
@@ -1,41 +1,60 @@
-(function () {
+/* global $ */
 
-    "use strict";
+'use strict';
 
-    var setPadding = function () {
+(function() {
 
-        var padding = ($('body').height() - $('.main').height()) / 2;
-
-        padding = Math.max(padding, 50)
-
-        $('.main').css({
-            'padding-top': Math.max(padding, 50)
-        });
-
-    };
-
-    var onload = function () {
-        cnt += 1;
-        if (cnt === 2) {
-            $('html').addClass('ready');
-        }
-    };
-
-    /*
-     * Preload images
-     */
-    var cnt = 0;
-
-    var imgs = [new Image, new Image];
-    imgs[0].onload = onload;
-    imgs[1].onload = onload;
-
-    imgs[0].src = "img/background.jpg";
-    imgs[1].src = "img/TRwebLogo.svg";
-
-    $(document).ready(function () {
-        setPadding()
-        $(window).on('resize', setPadding);
+  // vertical centering
+  var setPadding = function() {
+    var padding = ($('body').height() - $('.main').height()) / 2;
+    padding = Math.max(padding, 50);
+    $('.main').css({
+      'padding-top': Math.max(padding, 50)
     });
+  };
+
+  function setItemHeight() {
+    var height = Math.max.apply(null, $('.item').map(function() {
+      return $(this).height();
+    }));
+    $('.item').height(height);
+  }
+
+  $(document).ready(function() {
+    setPadding();
+    $(window).on('resize', setPadding);
+    setItemHeight();
+    $(window).on('resize', setItemHeight);
+  });
+
+  var onload = function() {
+    cnt += 1;
+    if (cnt === 2) {
+      $('html').addClass('ready');
+    }
+  };
+
+  /*
+   * Preload images
+   */
+  var cnt = 0;
+
+  var imgs = [new Image(), new Image()];
+  imgs[0].onload = onload;
+  imgs[1].onload = onload;
+
+  imgs[0].src = 'img/background-blur.jpg';
+  imgs[1].src = 'img/TRwebLogo.svg';
+
+  // change the highlighted nav link and move the background
+  $('.navbar-brand').click(function() {
+    var slideTo = $(this).data('slideTo');
+    $('.navbar-brand').removeClass('active');
+    $(this).addClass('active');
+    $('body')
+      .removeClass('slide0 slide1 slide2')
+      .addClass('slide' + slideTo);
+  });
+
 
 }());
